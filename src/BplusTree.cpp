f28e28f63560c8a5bea81958e3b4852d9afcefa4
panhe <mybestsonny@gmail.com>
2017-11-15 21:16:39 -0500

    remove boost library

--- src/BplusTree.cpp
+++ src/BplusTree.cpp
@@ -1,43 +1,26 @@
-#if !defined BPLUSTREE_HPP_227824
-#define BPLUSTREE_HPP_227824
+#if !defined BPLUSTREE_HPP
+#define BPLUSTREE_HPP
 
-// This is required for glibc to define std::posix_memalign
-#if !defined(_XOPEN_SOURCE) || (_XOPEN_SOURCE < 600)
-#define _XOPEN_SOURCE 600
-#endif
 #include <stdlib.h>
 #include <map>
 #include <stack>
 #include <assert.h>
-#include <boost/static_assert.hpp>
-#include <boost/pool/object_pool.hpp>
-
+#include <type_traits>
 // DEBUG
 #include <iostream>
 using std::cout;
 using std::endl;
 
-#if defined DEBUG_ASSERT
-#    warning "DEBUG_ASSERT overloaded"
-#else
-#    if defined DEBUG
-#        define DEBUG_ASSERT(expr) assert(expr)
-#    else
-#        define DEBUG_ASSERT(expr)
-#    endif
-#endif
 
-template <typename KEY, typename VALUE, unsigned N, unsigned M,
-	unsigned INNER_NODE_PADDING= 0, unsigned LEAF_NODE_PADDING= 0,
-	unsigned NODE_ALIGNMENT= 64>
+template <typename KEY, typename VALUE, unsigned N, unsigned M>
 class BPlusTree
 {
 public:
 	// N must be greater than two to make the split of
 	// two inner nodes sensible.
-	BOOST_STATIC_ASSERT(N>2);
+	static_assert(N>2, "N must be greater than two");
 	// Leaf nodes must be able to hold at least one element
-	BOOST_STATIC_ASSERT(M>0);
+	static_assert(M>0, "Leaf nodes must be able to hold at least one element");
 
 	// Builds a new empty tree.
 	BPlusTree()
@@ -61,14 +44,10 @@ public:
 		bool was_split;
 		if( depth == 0 ) {
 			// The root is a leaf node
-			DEBUG_ASSERT( *reinterpret_cast<NodeType*>(root) ==
-				NODE_LEAF);
 			was_split= leaf_insert(reinterpret_cast<LeafNode*>
 				(root), key, value, &result);
 		} else {
 			// The root is an inner node
-			DEBUG_ASSERT( *reinterpret_cast<NodeType*>
-				(root) == NODE_INNER );
 			was_split= inner_insert(reinterpret_cast<InnerNode*>
 				(root), depth, key, value, &result);
 		}
@@ -86,46 +65,6 @@ public:
 		}
 	}
 
-	// Looks for the given key. If it is not found, it returns false,
-	// if it is found, it returns true and copies the associated value
-	// unless the pointer is null.
-	bool find(const KEY& key, VALUE* value= 0) const {
-		InnerNode* inner;
-		register void* node= root;
-		register unsigned d= depth, index;
-		while( d-- != 0 ) {
-			inner= reinterpret_cast<InnerNode*>(node);
-			DEBUG_ASSERT( inner->type == NODE_INNER );
-			index= inner_position_for(key, inner->keys,
-				inner->num_keys);
-			node= inner->children[index];
-		}
-		LeafNode* leaf= reinterpret_cast<LeafNode*>(node);
-		DEBUG_ASSERT( leaf->type == NODE_LEAF );
-		index= leaf_position_for(key, leaf->keys, leaf->num_keys);
-		if( leaf->keys[index] == key ) {
-			if( value != 0 ) {
-				*value= leaf->values[index];
-			}
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	// Returns the size of an inner node
-	// It is useful when optimizing performance with cache alignment.
-	unsigned sizeof_inner_node() const {
-		return sizeof(InnerNode);
-	}
-
-	// Returns the size of a leaf node.
-	// It is useful when optimizing performance with cache alignment.
-	unsigned sizeof_leaf_node() const {
-		return sizeof(LeafNode);
-	}
-
-
   void getRange(const KEY& key1, const KEY& key2, std::multimap<KEY, VALUE>& res) {
     InnerNode* inner;
     register void* node= root;
@@ -140,10 +79,7 @@ public:
       inner= reinterpret_cast<InnerNode*>(node);
       // The children are leaf nodes
       for(unsigned kk=0; kk < inner->num_keys+1; ++kk) {
-        DEBUG_ASSERT( *reinterpret_cast<NodeType*>
-          (inner->children[kk]) == NODE_LEAF );
         LeafNode* leaf= reinterpret_cast<LeafNode*>(inner->children[kk]);
-        DEBUG_ASSERT( leaf->type == NODE_LEAF );
         int index = 0;
         while(index < leaf->num_keys) {
           if(leaf->keys[index] >= key1 && leaf->keys[index] <= key2) {
@@ -154,7 +90,6 @@ public:
       }
     }else{
       inner= reinterpret_cast<InnerNode*>(node);
-      DEBUG_ASSERT( inner->type == NODE_INNER );
       int index_lower = inner_position_for(key1, inner->keys, inner->num_keys);
       int index_higher = inner_position_for(key2, inner->keys, inner->num_keys);
       for(int index= index_lower; index <= index_higher; index++){
@@ -180,7 +115,6 @@ private:
 		unsigned num_keys;
 		KEY      keys[M];
 		VALUE    values[M];
-		unsigned char _pad[LEAF_NODE_PADDING];
 	};
 
 	// Inner nodes store pointers to other nodes interleaved with keys.
@@ -194,60 +128,24 @@ private:
 		unsigned num_keys;
 		KEY      keys[N];
 		void*    children[N+1];
-		unsigned char _pad[INNER_NODE_PADDING];
-	};
-
-	// Custom allocator that returns aligned blocks of memory
-	template <unsigned ALIGNMENT>
-	struct AlignedMemoryAllocator {
-		typedef std::size_t size_type;
-		typedef std::ptrdiff_t difference_type;
-
-		static char* malloc(const size_type bytes)
-		{
-			void* result;
-			if( posix_memalign(&result, ALIGNMENT, bytes) != 0 ) {
-				result= 0;
-			}
-			// Alternative: result= std::malloc(bytes);
-			return reinterpret_cast<char*>(result);
-		}
-		static void free(char* const block)
-		{ std::free(block); }
 	};
 
 	// Returns a pointer to a fresh leaf node.
 	LeafNode* new_leaf_node() {
 		LeafNode* result;
 		//result= new LeafNode();
-		result= leafPool.construct();
+		result= new LeafNode();
 		//cout << "New LeafNode at " << result << endl;
 		return result;
-	}
-
-	// Frees a leaf node previously allocated with new_leaf_node()
-	void delete_leaf_node(LeafNode* node) {
-		DEBUG_ASSERT( node->type == NODE_LEAF );
-		//cout << "Deleting LeafNode at " << node << endl;
-		// Alternatively: delete node;
-		leafPool.destroy(node);
 	}
 
 	// Returns a pointer to a fresh inner node.
 	InnerNode* new_inner_node() {
 		InnerNode* result;
 		// Alternatively: result= new InnerNode();
-		result= innerPool.construct();
+		result= new InnerNode();
 		//cout << "New InnerNode at " << result << endl;
 		return result;
-	}
-
-	// Frees an inner node previously allocated with new_inner_node()
-	void delete_inner_node(InnerNode* node) {
-		DEBUG_ASSERT( node->type == NODE_INNER );
-		//cout << "Deleting InnerNode at " << node << endl;
-		// Alternatively: delete node;
-		innerPool.destroy(node);
 	}
 
 	// Data type returned by the private insertion methods.
@@ -286,7 +184,6 @@ private:
 
 	bool leaf_insert(LeafNode* node, KEY& key,
 			VALUE& value, InsertionResult* result) {
-		DEBUG_ASSERT( node->type == NODE_LEAF );
 		assert( node->num_keys <= M );
 		bool was_split= false;
 		// Simple linear search
@@ -324,7 +221,6 @@ private:
 
 	static void leaf_insert_nonfull(LeafNode* node, KEY& key, VALUE& value,
 			unsigned index) {
-		DEBUG_ASSERT( node->type == NODE_LEAF );
 		assert( node->num_keys < M );
 		assert( index <= M );
 		assert( index <= node->num_keys );
@@ -346,7 +242,6 @@ private:
 
 	bool inner_insert(InnerNode* node, unsigned depth, KEY& key,
 			VALUE& value, InsertionResult* result) {
-		DEBUG_ASSERT( node->type == NODE_INNER );
 		assert( depth != 0 );
 		// Early split if node is full.
 		// This is not the canonical algorithm for B+ trees,
@@ -386,7 +281,6 @@ private:
 
 	void inner_insert_nonfull(InnerNode* node, unsigned depth, KEY& key,
 			VALUE& value) {
-		DEBUG_ASSERT( node->type == NODE_INNER );
 		assert( node->num_keys < N );
 		assert( depth != 0 );
 		// Simple linear search
@@ -396,18 +290,10 @@ private:
 		bool was_split;
 		if( depth-1 == 0 ) {
 			// The children are leaf nodes
-			for(unsigned kk=0; kk < node->num_keys+1; ++kk) {
-				DEBUG_ASSERT( *reinterpret_cast<NodeType*>
-					(node->children[kk]) == NODE_LEAF );
-			}
 			was_split= leaf_insert(reinterpret_cast<LeafNode*>
 				(node->children[index]), key, value, &result);
 		} else {
 			// The children are inner nodes
-			for(unsigned kk=0; kk < node->num_keys+1; ++kk) {
-				DEBUG_ASSERT( *reinterpret_cast<NodeType*>
-					(node->children[kk]) == NODE_INNER );
-			}
 			InnerNode* child= reinterpret_cast<InnerNode*>
 				(node->children[index]);
 			was_split= inner_insert( child, depth-1, key, value,
@@ -436,13 +322,6 @@ private:
 		} // else the current node is not affected
 	}
 
-	typedef AlignedMemoryAllocator<NODE_ALIGNMENT> AlignedAllocator;
-
-	// Node memory allocators. IMPORTANT NOTE: they must be declared
-	// before the root to make sure that they are properly initialised
-	// before being used to allocate any node.
-	boost::object_pool<InnerNode, AlignedAllocator> innerPool;
-	boost::object_pool<LeafNode, AlignedAllocator>  leafPool;
 	// Depth of the tree. A tree of depth 0 only has a leaf node.
 	unsigned depth;
 	// Pointer to the root node. It may be a leaf or an inner node, but
@@ -450,4 +329,4 @@ private:
 	void*    root;
 };
 
-#endif // !defined BPLUSTREE_HPP_227824
+#endif
